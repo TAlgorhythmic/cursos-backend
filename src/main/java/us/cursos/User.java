9e72e97138b2fb31d79509d74f39c8248cc2b919
Nayra Rios <nayrios2004@gmail.com>
2024-07-03 19:32:24 +0200

    dddddd

---
+++ src/main/java/us/cursos/User.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package us.cursos;
+
+/**
+ *
+ * @author nayri
+ */
+public class User {
+    private int password;
+    private String username;
+    private String gmail;
+    private String name;
+    private String lastName;
+    private int d=1;
+   
+}
